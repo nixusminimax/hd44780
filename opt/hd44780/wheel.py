5cbb3a6eeda80422968b45f669e54e22b35bc6b6
nixusminimax <nixusminimax@users.noreply.github.com>
2016-06-20 21:07:14 +0200

    bye bye subprocess
    much nicer without using subprocess ;-)

--- opt/hd44780/wheel.py
+++ opt/hd44780/wheel.py
@@ -1,5 +1,7 @@
+#! /usr/bin/python
+
 import config
-from evdev import InputDevice
+import evdev
 from select import select
 import socket
 import subprocess
@@ -7,23 +9,27 @@ import sys
 import telnetlib
 import time
 
+
 ## mousedetection
-my_mouse = subprocess.check_output("grep 'mouse' /proc/bus/input/devices |cut -d ' ' -f3 |tr '\r\n' ' ' ", shell=True)
-my_mouse = my_mouse.replace(" ", "")
+my_mouse = ''
+
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+for device in devices:
+    if "Mouse" in device.name:
+        my_mouse = device.fn
 
 if my_mouse == '':
    print "no mouse detected"
    sys.exit(0)
 
-dev = evdev.InputDevice('/dev/input/' + (my_mouse))
-
+mouse = evdev.InputDevice('' + (my_mouse))
 
 ## Logitechmediaserverstuff
 def lms_unaviable():  
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((config.lms,config.cliport))   
     return result
-                
+                            
 def lms_feedback(mac,question):
     tn = telnetlib.Telnet(config.lms, config.cliport)
     tn.write(config.mac + " " + question + " ?\n")   
@@ -34,55 +40,57 @@ def lms_feedback(mac,question):
     answer = answer.replace(mac+" "+question+' ','')
     result = answer.replace(mac+" "+question,'')
     return result
-
+                                                                
 def lms_set_value(mac,set,value):
     tn = telnetlib.Telnet(config.lms, config.cliport)
     print (config.mac + " " + set + " " + value + " \n")
     tn.write(config.mac + " " + set + " " + value + " \n")
     tn.write("exit\n")    
-    
-
+                                                                                    
+                                                                                    
 while True:
-       info = ['mixer volume']
-       for question in info:
-               if not lms_unaviable():
-                  r,w,x = select([dev], [], [])       
-                  answer = lms_feedback(config.mac,question)
-                  for event in dev.read():
-                      #Wheel spinning
-                      if event.code == 8:
-                         vol = int(answer)
-                         #Volume Down
-                         if event.value == -1: 
-                             if vol >= 80:
-                                vol = vol - 2
-                             elif vol >= 50:
-                                vol = vol - 10
-                                   
-                             if vol != 0:
-                                vol = vol -5
-                             new_vol = str(vol)                             
-                             lms_set_value(config.mac,'mixer volume',new_vol) 
-                         ##Volume Up
-                         elif event.value == 1:
-                             if vol <= 50:
-                                vol = vol +10
-                             elif vol <= 80:
-                                vol = vol +5
-                             elif vol != 100:
-                                vol = vol + 1
-                             new_vol = str(vol)
-                             lms_set_value(config.mac,'mixer volume',new_vol)
-                                                                               
-                      #Left Button pressed
-                      elif event.code == 272 and event.value == 1:
-                           lms_set_value(config.mac,'playlist index','-1')
-                      #Right Button pressed       
-                      elif event.code == 273 and event.value == 1:
-                           lms_set_value(config.mac,'playlist index','+1')
-                      # Mouse Wheel pressed
-                      elif event.code == 274 and event.value == 1:
-                           lms_set_value(config.mac,'pause', '')
-                           #prevent to pause and unpause when the wheel is pressed a long time
-                           time.sleep (2)                                                     
-                             
+      info = ['mixer volume']
+      for question in info:
+          if not lms_unaviable():
+             r,w,x = select([mouse], [], [])       
+             answer = lms_feedback(config.mac,question)
+             for event in mouse.read():
+                 #Wheel spinning
+                 if event.code == 8:
+                    vol = int(answer)
+                        
+                 #Volume Down
+                    if event.value == -1: 
+                       if vol >= 80:
+                          vol = vol - 2
+                       elif vol >= 50:
+                             vol = vol - 10                                                                                                                                                                                                                                                                                                                                                                                                                             
+                       elif vol != 0:
+                             vol = vol -5
+                       new_vol = str(vol)                             
+                       lms_set_value(config.mac,'mixer volume',new_vol) 
+                        
+                 #Volume Up
+                    elif event.value == 1:
+                         if vol <= 50:
+                            vol = vol +10
+                         elif vol <= 80:
+                              vol = vol +5
+                         elif vol != 100:
+                              vol = vol + 1
+                         new_vol = str(vol)
+                         lms_set_value(config.mac,'mixer volume',new_vol)
+                                                                                    
+                 #Left Button pressed
+                    elif event.code == 272 and event.value == 1:
+                         lms_set_value(config.mac,'playlist index','-1')
+                        
+                 #Right Button pressed       
+                    elif event.code == 273 and event.value == 1:
+                         lms_set_value(config.mac,'playlist index','+1')
+                        
+                 # Mouse Wheel pressed
+                    elif event.code == 274 and event.value == 1:
+                         lms_set_value(config.mac,'pause', '')
+                         #prevent to pause and unpause when the wheel is pressed a long time
+                         time.sleep (2)
